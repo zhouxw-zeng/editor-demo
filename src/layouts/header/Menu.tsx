21a7386b1c70e206c2f2b9154a2b42fed1a9ca1f
Avin <diuwenz@163.com>
2025-02-28 08:50:23 +0800

    feat: 增加头部路由

--- src/layouts/header/Menu.tsx
+++ src/layouts/header/Menu.tsx
@@ -14,12 +14,33 @@ import UserTools from "./UserTools"
 import Link from "next/link"
 export default function HeaderMenu() {
   return (
-    <div className="h-16 flex w-full items-center justify-between gap-2 px-4 py-2 border-b-2 shadow-sm">
-      <NavigationMenu>
+    <div className="fixed h-16 flex w-full items-center justify-between gap-2 px-4 py-2 border-b-2 shadow-sm list-none">
+      <NavigationMenu >
         <NavigationMenuItem>
           <Link href="/" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/about" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              About
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/notebook" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            Notebook
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/blog" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            Blog
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
