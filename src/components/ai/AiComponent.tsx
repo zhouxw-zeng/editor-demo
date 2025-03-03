3be1c7f4163f58f957460e7646a7cc34ae37c2a5
Avin <diuwenz@163.com>
2025-03-03 11:59:57 +0800

    feat: optimize Ai Components

--- src/components/ai/AiComponent.tsx
+++ src/components/ai/AiComponent.tsx
@@ -1,21 +1,30 @@
-import { SquareChevronRight } from "lucide-react"
-
+import { Button } from "../ui/button";
+import Magic from "@/components/ui/magic";
+import Clipboard from "@/components/ui/clipboard";
 interface AiComponent {
   onClick: (isEnabled: boolean) => void;
 }
 import "./ai.css"
 const AiComponent = ({onClick}: AiComponent) => {
   return (
-    <div className="min-w-fit ai-heihgt border-l-2 shadow-sm flex flex-col">
+    <div>
+      <div className="min-w-fit ai-heihgt border-l-2 shadow-sm flex flex-col">
       <div className="h-12 flex justify-between items-center">
-        <div className="flex-1">这是标题</div>
-        {/* <SquareChevronRight className="h-2 w-2 p-2 custom-icon-color" /> */}
-        {/* 临时关闭 */}
-        <div className="cursor-pointer" onClick={() => onClick(false)}>
-          关闭
+        <div className="flex-1 cursor-pointer font-bold">
+          <Clipboard copy={"这个是标题哇"} />
         </div>
+        <Button
+          className="gap-1 rounded-none text-green-500 hover:text-red-500"
+          variant="ghost"
+          onClick={() => onClick(false)}
+          >
+          <Magic className="w-10 h-10" />
+          Off AI
+        </Button>
       </div>
     </div>
+    </div>
+    
   )
 }
 export default AiComponent
--- src/components/ai/AiSwitch.tsx
+++ src/components/ai/AiSwitch.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
+import Magic from "@/components/ui/magic"
 
 interface MyComponentProps {
   onClick: (e: boolean) => void // 通用的函数类型
@@ -10,11 +12,19 @@ interface MyComponentProps {
 const AiSwitch = ({ onClick }: MyComponentProps) => {
   return (
       <div
-        className="fixed top-8 right-1/3 z-20 cursor-pointer bg-transparent border-none"
-        onClick={() => onClick(true)}
+        className="fixed top-6 right-1/3 z-20 cursor-pointer bg-transparent border-none"
         aria-label="开启 AI 功能"
       >
-        开启
+        <Button
+          className="gap-1 rounded-xl text-green-500 hover:text-cyan-300"
+          variant="ghost"
+          onClick={() => onClick(true)}
+          aria-label="开启 AI 功能"
+          size="sm"
+        >
+          <Magic className="h-5 w-5" />
+          Open AI
+        </Button>
       </div>
   )
 }
